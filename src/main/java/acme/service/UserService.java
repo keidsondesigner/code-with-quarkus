585ebfbf1e0f8c51875e2f1fe9b02d7212367184
keidsondesigner <keidsondesigner@gmail.com>
2026-06-18 14:34:09 -0400

    feat(users): Criado service findAllUsers

--- src/main/java/acme/service/UserService.java
+++ src/main/java/acme/service/UserService.java
@@ -1,12 +1,19 @@
 package acme.service;
 
+import java.util.List;
+
 import acme.domain.Users;
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class UserService {
+
   public Users createUser(Users users) {
     Users.persist(users);
     return users;
+  }
+
+  public List<Users> findAllUsers(Integer page, Integer size) {
+    return Users.findAll().page(page, size).list();
   }
 }
